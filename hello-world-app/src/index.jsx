72a6f3f8ce24fb1f6ca7beeae44c64eb2e401909
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2024-01-13 17:10:25 +0300

    Update index.jsx

--- hello-world-app/src/index.jsx
+++ hello-world-app/src/index.jsx
@@ -1,5 +1,6 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel,useProductContext, useState, useEffect } from '@forge/ui';
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState, Tooltip, Link, Button, React, TooltipProps } from '@forge/ui';
 import api, { route } from "@forge/api";
+
 
 const fetchCommentsForIssue = async (issueId) => {
   const res = await api
@@ -11,28 +12,41 @@ const fetchCommentsForIssue = async (issueId) => {
 };
 
 
+
 const App = () => {
   const context = useProductContext();
   const [comments] = useState(async () => await fetchCommentsForIssue(context.platformContext.issueKey));
+  //console.log("All comments ", comments)
 
   console.log(`Number of comments on this issue: ${comments.length}`);
-  console.log("comments author :", comments[0].author.displayName)
-  console.log("comments date : ", comments[0].created)
 
-  return (
-    <Fragment>
-      <Text>Hello world!</Text>
-      <Text>
-        Number of comments on this issue: {comments.length}
-      </Text>
-      <Text>
-        Owner of the last comment: {comments[comments.length-1].author.displayName}  
-      </Text>
-      <Text>
-        Date and time of the last comment: {comments[comments.length-1].created}
-      </Text>
-    </Fragment>
-  );
+  if (comments.length > 0) {
+    const lastComment = comments[comments.length - 1];
+    console.log("comments author:", comments[0].author.displayName);
+    console.log("comments date:", comments[0].created);
+
+    return (
+      <Fragment>
+        <Text>Hello world ! </Text>
+        <Text>
+          Number of comments on this issue: {comments.length}
+        </Text>
+        <Text>
+          Owner of the last comment: {lastComment.created}
+        </Text>
+        <Tooltip text={"Detail Panel"}>
+          <Text>Owner of the last comment: {lastComment.author.displayName}</Text>
+        </Tooltip>
+      </Fragment >
+    );
+  } else {
+    // Eğer yorum yoksa
+    return (
+      <Fragment>
+        <Text>Hello world! No comments on this issue.</Text>
+      </Fragment>
+    );
+  }
 };
 
 export const run = render(
@@ -40,6 +54,5 @@ export const run = render(
     <App />
   </IssuePanel>
 );
-
 
 
